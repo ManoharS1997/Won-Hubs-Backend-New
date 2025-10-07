e758972587e38c99b454a5ab0a3eddf32d6908e6
SandhyaChattu2426 <sandhya.chattu@nowitservices.com>
2025-10-07 10:42:07 +0530

    Delete routes directory

--- routes/Partners/PartnerRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router()
-const partnerControllers = require("../../controllers/partnerControllers/partnerControllers")
-
-
-router.post('/request/partnership', partnerControllers.CreatePartner)
-
-module.exports = router
