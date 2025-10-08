a0bc8c256e3d2bb86914d59932f8acd4c77fb76a
SandhyaChattu2426 <sandhya.chattu@nowitservices.com>
2025-10-08 10:58:32 +0530

    Delete utils directory

--- utils/convertName.js
+++
@@ -1,8 +0,0 @@
-function convertName(name) {
-    if (name === '') return
-    const nameArr = name.split('_')
-    const convertedName = nameArr.map((item,) => item[0].toUpperCase() + item.slice(1))
-    return (convertedName.join(' '))
-}
-
-module.exports = convertName
